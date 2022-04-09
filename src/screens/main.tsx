d705947cd19b6c6bc6adade321fa9811a0a0d20e
FedjaW <f.wilms89@web.de>
2022-04-09 19:00:45 +0200

    added checkmark component

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,8 +1,15 @@
-import * as React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Text, Box, Center, VStack, useColorModeValue } from 'native-base'
 import ThemeToggle from '../componentes/theme-toggle'
+import AnimatedCheckBox from '../componentes/animated-checkbox'
+import { Pressable } from 'react-native'
 
 const MainScreen = () => {
+  const [checked, setChecked] = useState(false)
+  const handlePressCheckBox = useCallback(() => {
+    setChecked(prev => !prev)
+  }, [])
+
   return (
     <Center
       _dark={{ bg: 'blueGray.900' }}
@@ -11,6 +18,11 @@ const MainScreen = () => {
       flex={1}
     >
       <VStack space={5} alignItems="center">
+        <Box w="100px" h="100px">
+          <Pressable onPress={handlePressCheckBox}>
+            <AnimatedCheckBox checked={checked} />
+          </Pressable>
+        </Box>
         <Box p={10} bg={useColorModeValue('red.500', 'yellow.500')}>
           <Text>Hello</Text>
         </Box>
